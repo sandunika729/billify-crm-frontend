8f763ae07796f8787fe933704405b384f34eb54a
Sandunika Karunadasa <sandunikathilakshani2k@gmail.com>
2026-06-22 19:30:26 +0530

    add responsiveness

--- frontend/src/components/modals/Modal.js
+++ frontend/src/components/modals/Modal.js
@@ -18,11 +18,14 @@ export default function Modal({
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = 'unset';
+      document.documentElement.style.overflow = 'unset';
     }
     return () => {
       document.body.style.overflow = 'unset';
+      document.documentElement.style.overflow = 'unset';
     };
   }, [isOpen]);
 
